0e76ef7f496c19e6ec3afe269b92996213da66c3
Ricardo Barbosa <ricardobsaf@gmail.com>
2020-05-03 13:28:34 -0300

    First screen.

---
+++ junket/app.js
@@ -0,0 +1,28 @@
+// Node hub 2020
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+var listener = app.listen(8080, function() {
+  console.log("Listening on port " + listener.address().port);
+});
