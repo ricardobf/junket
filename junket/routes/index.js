df039f0fd20fe8494698af80982c654d26675930
Ricardo Barbosa <ricardobsaf@gmail.com>
2020-05-23 17:36:45 -0300

    Add ldapjs module.

--- junket/routes/index.js
+++ junket/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var ldap = require('ldapjs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,7 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 router.post("/subscribe", function(req, res, next) {
-  const { name, email } = req.body;
+  const { login, password } = req.body;
 
   // 1. Validate the user data
   // 2. Subscribe the user to the mailing list
@@ -17,7 +18,41 @@ router.post("/subscribe", function(req, res, next) {
 
   res.render("subscribed", {
     title: "You are subscribed",
+    login,
+    password
+  });
+});
+
+router.post("/admin", function(req, res, next) {
+  const { name, email } = req.body;
+
+  // 1. Validate the user data
+  // 2. Subscribe the user to the mailing list
+  // 3. Send a confirmation email
+
+  res.render("admin", {
+    title: "Welcome",
     name,
     email
   });
 });
+
+router.post("/user", function(req, res, next) {
+  const { login, password } = req.body;
+
+  
+  var client = ldap.createClient({
+    url: 'ldap://orb.sense.dcc.ufmg.br:389'
+  });
+
+  client.bind('cn=ricardobarbosa', 'Junketws4', function(err) {
+    assert.ifError(err);
+  });
+
+  res.render("user", {
+    title: "Welcome",
+    login,
+    password
+  });
+});
+
